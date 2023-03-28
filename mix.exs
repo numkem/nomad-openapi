a47b0a76f160e503f088030ab5b0a7d9e01913ce
Sebastien Bariteau <sebastien.bariteau@methodstudios.com>
2023-03-28 11:20:27 -0400

    initial commit

---
+++ mix.exs
@@ -0,0 +1,42 @@
+defmodule Nomad.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+     app: :nomad,
+     version: "1.1.4",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
+     elixir: "~> 1.11",
+     start_permanent: Mix.env == :prod,
+     deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    # Specify extra applications you'll use from Erlang/Elixir
+    [extra_applications: [:logger]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:my_dep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.3.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [
+      {:tesla, "~> 1.4.0"},
+      {:hackney, "~> 1.17"},
+      {:poison, "~> 3.0"}
+    ]
+  end
+end
